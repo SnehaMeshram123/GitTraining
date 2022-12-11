71f29ff48cdbcb1e28ec182d9e8204e9dbef8bcf
VelocityTraining1 <pnnarve9@gmail.com>
2022-12-11 12:23:32 +0530

    Fifth commit

--- src/Arraydemo/example1.java
+++ src/Arraydemo/example1.java
@@ -28,7 +28,7 @@ public class example1 {
 		System.out.println(arr[5]);
 		System.out.println(arr[6]);
 		System.out.println(arr[6]);
-		
+		System.out.println(arr[6]);
 		//using for loop
 		for(int i=0;i<=4;i++)
 		{
